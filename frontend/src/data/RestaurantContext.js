279662292f4b83f9abcec9a026b2b121494cb36d
jaredden <james.ifit@gmail.com>
2023-10-25 15:25:44 -0400

    connecting local repo to remote repo

--- frontend/src/data/RestaurantContext.js
+++ frontend/src/data/RestaurantContext.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
-const RestaurantContext = createContext();
+// import { createContext } from "react";
+// const RestaurantContext = createContext();
 
-export { RestaurantContext };
+// export { RestaurantContext };
